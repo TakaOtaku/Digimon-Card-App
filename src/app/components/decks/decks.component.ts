17be880ec39014b23c1c63d2f78259ca1a708b42
ChristianBayer <Strohhut1149135>
2022-03-08 23:09:28 +0100

    Deck Changes

--- src/app/components/decks/decks.component.ts
+++ src/app/components/decks/decks.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 import {MatDialog} from "@angular/material/dialog";
 import {IDeck} from "../../models";
 import {ImportDeckComponent} from "../import-deck/import-deck.component";
@@ -6,16 +6,52 @@ import {ImportDeckComponent} from "../import-deck/import-deck.component";
 @Component({
   selector: 'digimon-decks',
   templateUrl: './decks.component.html',
-  styleUrls: ['./decks.component.css']
+  styleUrls: ['./decks.component.scss'],
 })
 export class DecksComponent implements OnInit {
-  public decks: IDeck[] = [{
+  public decks: IDeck[] = [
+    {
+    cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
+    title: 'Titamon Deck',
+    description: '[P] Titamon Deck for BT7',
+    color: '#6256a5',
+    image: 'assets/images/cards/BT6-081_P1.png'
+    },
+    {
+    cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
+    title: 'Titamon Deck',
+    description: '[P] Titamon Deck for BT7',
+    color: '#fee000',
+    image: 'assets/images/cards/BT6-081_P1.png'
+    },
+    {
     cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
     title: '[P] Titamon Deck',
     description: '[P] Titamon Deck for BT7',
-    color: 'purple',
+    color: '#009c6b',
     image: 'assets/images/cards/BT6-081_P1.png'
-  }];
+    },
+    {
+    cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
+    title: '[P] Titamon Deck',
+    description: '[P] Titamon Deck for BT7',
+    color: '#e70530',
+    image: 'assets/images/cards/BT6-081_P1.png'
+    },
+    {
+    cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
+    title: '[P] Titamon Deck',
+    description: '[P] Titamon Deck for BT7',
+    color: '#0096e0',
+    image: 'assets/images/cards/BT6-081_P1.png'
+    },
+    {
+    cards: [{count: 4, name: 'Titamon', id: 'BT6-81'}],
+    title: '[P] Titamon Deck',
+    description: '[P] Titamon Deck for BT7',
+    color: '#201813',
+    image: 'assets/images/cards/BT6-081_P1.png'
+    }];
 
   constructor(public dialog: MatDialog) { }
 
@@ -23,6 +59,6 @@ export class DecksComponent implements OnInit {
   }
 
   public openImportDialog(): void  {
-    this.dialog.open(ImportDeckComponent, {width: '70%', height: '50%'});
+    this.dialog.open(ImportDeckComponent, {width: '600px', height: '700px'});
   }
 }
--- src/app/components/import-deck/import-deck.component.ts
+++ src/app/components/import-deck/import-deck.component.ts
@@ -1,15 +1,43 @@
 import {Component, OnInit} from '@angular/core';
+import {ISave} from "../../models";
+import {loadSave} from "../../store/actions/save.actions";
 
 @Component({
   selector: 'digimon-import-deck',
   templateUrl: './import-deck.component.html',
   styleUrls: ['./import-deck.component.css']
 })
-export class ImportDeckComponent implements OnInit {
+export class ImportDeckComponent {
+  public importPlaceholder = "" +
+    "Paste Deck here\n" +
+    "\n" +
+    " Format:\n" +
+    "   Qty Name Id\n" +
+    "   Name is optional. Qty(quantity) must be positiv\n" +
+    "   Each card can only be declared once\n" +
+    "   Import will always pick the regular art card\""
 
+  public deckText = '';
+
+  //Import -> Ask for Name, Description, Color, SelectCardID for Image or let empty
   constructor() { }
 
-  ngOnInit(): void {
+  public handleFileInput(input: any) {
+    let fileReader = new FileReader();
+    fileReader.onload = (e) => {
+      try {
+        console.log(fileReader.result as string);
+        debugger;
+        //const save: ISave = JSON.parse(fileReader.result as string);
+        //this.store.dispatch(loadSave({save}));
+      } catch (e) {
+
+      }
+    }
+    fileReader.readAsText(input.files[0]);
   }
 
+  public importDeck() {
+
+  }
 }
