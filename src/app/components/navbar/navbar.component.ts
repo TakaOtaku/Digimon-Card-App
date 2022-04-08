97d2de1de99fa3c0a40532963d32c4d386812748
ChristianBayer <Strohhut1149135>
2022-04-08 10:31:51 +0200

    Mobile can't change Card-Sizes now, as it is set to max. Full-Card-Count is now displayed properly on mobile.

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,11 +1,20 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Store} from "@ngrx/store";
+import {changeCardSize} from "../../store/digimon.actions";
 
 @Component({
   selector: 'digimon-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
   @Input() public compact = false;
   @Input() public site: number = 0;
+
+  constructor(private store: Store) {}
+
+  ngOnInit() {
+    if(!this.compact) {return}
+    this.store.dispatch(changeCardSize({cardSize: 100}))
+  }
 }
