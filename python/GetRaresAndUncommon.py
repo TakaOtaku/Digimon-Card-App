8057fec9d4d82c4ac9b1bb96bcb02417df7d50c4
TakaOtaku <christian_bayer@yahoo.de>
2022-05-08 18:04:44 +0200

    Py Script Error

--- python/GetRaresAndUncommon.py
+++ python/GetRaresAndUncommon.py
@@ -6,21 +6,23 @@ import re
 import pandas as pd
 
 
-cards = list()
-engCards = []
-
-with open('BT8.json', encoding='utf-8') as fh:
-    engCards = json.load(fh)
-
-for card in engCards:
-	if card['version'] == 'Normal':
-		if card['rarity'] == 'R' or card['rarity'] == 'UC':
-			card['id']  = card['id'] + '_P0'
-			card['cardImage']  = re.sub(r'eng', 'pre-release/png', card['cardImage'])
-			card['cardImage']  = re.sub(r'.jpg', '_P0.jpg', card['cardImage'])
-			card['version']  = 'Pre-Release'
-			cards.append(card)
-
-with open('BT8.json', 'w') as f:
-    for card in cards:
-        f.write("%s\n" % json.dumps(card))
+names = ['BT4','BT5','BT6','BT7','BT8']
+for name in names:
+	cards = list()
+	engCards = []
+	
+	with open(name+'.json', encoding='utf-8') as fh:
+	    engCards = json.load(fh)
+	
+	for card in engCards:
+		if card['version'] == 'Normal':
+			if card['rarity'] == 'R' or card['rarity'] == 'U':
+				card['id']  = card['id'] + '_P0'
+				card['cardImage']  = re.sub(r'eng', 'pre-release/png', card['cardImage'])
+				card['cardImage']  = re.sub(r'.jpg', '_P0.jpg', card['cardImage'])
+				card['version']  = 'Pre-Release'
+				cards.append(card)
+	
+	with open(name+'.json', 'w') as f:
+	    for card in cards:
+	        f.write("%s\n" % json.dumps(card))
