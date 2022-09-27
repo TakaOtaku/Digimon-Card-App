7a4faab5b66a77fdd5042f50946cc0b7e481edf5
TakaOtaku <christian_bayer@yahoo.de>
2022-09-27 20:26:39 +0200

    Changelog

---
+++ src/app/components/molecules/dialogs/changelog-dialog/changelog-dialog.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'digimon-changelog-dialog',
+  templateUrl: './changelog-dialog.component.html'
+})
+export class ChangelogDialogComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
