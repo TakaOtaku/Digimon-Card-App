65e441c5b44a610d17d24acf9151f435c4592dd4
TakaOtaku <christian_bayer@yahoo.de>
2022-10-17 22:02:01 +0200

    BT10 AAs

--- src/models/tierlist.data.ts
+++ src/models/tierlist.data.ts
@@ -1,37 +1,37 @@
 export const TIERLIST = {
   S: [
-    { name: 'Xros Heart', image: 'assets/images/cards/jap/BT10-009.webp' }, // Xros Heart
+    { name: 'Xros Heart', image: 'assets/images/cards/jap/BT10-013.webp' }, // Xros Heart
     { name: 'MetalGarurumon X', image: 'assets/images/cards/eng/BT9-031.webp' }, // MetalGaruru X
     { name: 'Alphamon', image: 'assets/images/cards/eng/BT9-111.webp' }, // Alphamon
-    { name: 'Minervamon', image: 'assets/images/cards/jap/BT10-083.webp' }, // Minervamon
   ],
   A: [
     { name: 'Gaiomon', image: 'assets/images/cards/eng/BT9-068.webp' }, // Gaiomon
     { name: 'WarGreymon X', image: 'assets/images/cards/eng/BT9-016.webp' }, // WarGreymon
     { name: 'GrandisKuwagamon', image: 'assets/images/cards/eng/BT9-055.webp' }, // GrandisKuwagamon
-    { name: 'D-Reaper', image: 'assets/images/cards/eng/EX2-007.webp' }, // D-Reaper
-    { name: 'Imperialdramon', image: 'assets/images/cards/eng/BT8-032.webp' }, // Imperialdramon
     { name: 'Yellow Hybrid', image: 'assets/images/cards/eng/BT7-038.webp' }, // Yellow Hybrid
-    { name: 'Jesmon', image: 'assets/images/cards/eng/BT6-016.webp' }, // Jesmon
+    { name: 'Jesmon', image: 'assets/images/cards/eng/ST12-10.webp' }, // Jesmon
     { name: 'Mastemon', image: 'assets/images/cards/eng/ST10-06.webp' }, // Mastemon
-    { name: 'Magnamon', image: 'assets/images/cards/eng/BT9-044.webp' }, // Magnamon
-    { name: 'Beelstarmon', image: 'assets/images/cards/eng/BT6-112.webp' }, // Beelstarmon
+    { name: 'Magnamon X', image: 'assets/images/cards/eng/BT9-044.webp' }, // Magnamon
+    { name: 'Blue Flare', image: 'assets/images/cards/jap/BT10-026.webp' }, // Blue Flare
+    { name: 'Minervamon', image: 'assets/images/cards/jap/BT10-083.webp' }, // Minervamon
+    {
+      name: 'Sakuyamon Maid Mode',
+      image: 'assets/images/cards/jap/BT10-041.webp',
+    }, // Sakuyamon Maid Mode
   ],
   B: [
-    { name: 'D-Brigade', image: 'assets/images/cards/eng/BT4-074.webp' }, // D-Brigade
     { name: 'Green Hybrid', image: 'assets/images/cards/eng/BT7-051.webp' }, // Green Hybrid
     { name: 'BloomLordmon', image: 'assets/images/cards/jap/BT10-057.webp' }, // BloomLordmon
     { name: 'DarkKnightmon', image: 'assets/images/cards/jap/BT10-069.webp' }, // Darknightmon
     { name: 'Beelzemon', image: 'assets/images/cards/eng/EX2-074.webp' }, // Beelzemon
     { name: 'Gallantmon', image: 'assets/images/cards/eng/EX2-073.webp' }, // Gallantmon
-    { name: 'Blue Flare', image: 'assets/images/cards/jap/BT10-026.webp' }, // Blue Flare
-    {
-      name: 'Sakuyamon Maid Mode',
-      image: 'assets/images/cards/jap/BT10-041.webp',
-    }, // Sakuyamon Maid Mode
-    { name: 'Diaboromon', image: 'assets/images/cards/eng/P-016.webp' }, // Diaboromon
+    { name: 'Beelstarmon', image: 'assets/images/cards/eng/BT6-112.webp' }, // Beelstarmon
+    { name: 'D-Reaper', image: 'assets/images/cards/eng/EX2-007.webp' }, // D-Reaper
+    { name: 'Imperialdramon', image: 'assets/images/cards/eng/BT8-032.webp' }, // Imperialdramon
   ],
   C: [
     { name: 'Etemon', image: 'assets/images/cards/eng/BT3-070.webp' }, // Etemon
+    { name: 'D-Brigade', image: 'assets/images/cards/eng/BT4-074.webp' }, // D-Brigade
+    { name: 'Diaboromon', image: 'assets/images/cards/eng/P-016.webp' }, // Diaboromon
   ],
 };
