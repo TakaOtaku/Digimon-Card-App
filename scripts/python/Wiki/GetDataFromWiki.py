74021d0c5646d841b0b8bac35cb6c8346db6cd15
TakaOtaku <christian_bayer@yahoo.de>
2025-02-10 18:58:01 +0100

    Get Rulings Again

--- scripts/python/Wiki/GetDataFromWiki.py
+++ scripts/python/Wiki/GetDataFromWiki.py
@@ -35,7 +35,7 @@ WikiVariables.cardCount = len(WikiVariables.cardLinks)
 print('Getting Card Data!')
 GetCardData.getCardData()
 
-# WF.getRulings()
+WF.getRulings()
 
 print('Saving Cards!')
 WikiFunctions.saveCards()
