058a50b91e3e900edbb587c69a5678cee1a6a016
tari <104586582+tari101190@users.noreply.github.com>
2026-03-26 06:35:26 +0000

    Update tags.data.ts

--- src/models/data/tags.data.ts
+++ src/models/data/tags.data.ts
@@ -1,6 +1,7 @@
 import { ITag } from '../interfaces';
 
 export const TAGS = [
+  'AD1',
   'BT1',
   'BT2',
   'BT3',
@@ -25,8 +26,8 @@ export const TAGS = [
   'BT22',
   'BT23',
   'BT24',
+  'BT25',
   'RB1',
-  'RB2',
   'LM',
   'EX1',
   'EX2',
@@ -39,6 +40,7 @@ export const TAGS = [
   'EX9',
   'EX10',
   'EX11',
+  'EX12',
   'ST1',
   'ST2',
   'ST3',
@@ -60,6 +62,8 @@ export const TAGS = [
   'ST20',
   'ST21',
   'ST22',
+  'ST23',
+  'ST24',
 ];
 
 export const tagsList: ITag[] = [
@@ -87,6 +91,7 @@ export const tagsList: ITag[] = [
   { name: 'BT22', color: 'Primary' },
   { name: 'BT23', color: 'Primary' },
   { name: 'BT24', color: 'Primary' },
+  { name: 'BT25', color: 'Primary' },
 
   { name: 'EX1', color: 'Primary' },
   { name: 'EX2', color: 'Primary' },
@@ -98,9 +103,11 @@ export const tagsList: ITag[] = [
   { name: 'EX8', color: 'Primary' },
   { name: 'EX9', color: 'Primary' },
   { name: 'EX10', color: 'Primary' },
+  { name: 'EX11', color: 'Primary' },
+  { name: 'EX12', color: 'Primary' },
 
+  { name: 'AD1', color: 'Primary' },
   { name: 'RB1', color: 'Primary' },
-  { name: 'RB2', color: 'Primary' },
 
   { name: 'LM', color: 'Primary' },
 
@@ -126,6 +133,8 @@ export const tagsList: ITag[] = [
   { name: 'ST20', color: 'Primary' },
   { name: 'ST21', color: 'Primary' },
   { name: 'ST22', color: 'Primary' },
+  { name: 'ST23', color: 'Primary' },
+  { name: 'ST23', color: 'Primary' },
 
   { name: 'Illegal', color: 'Primary' },
 ];
